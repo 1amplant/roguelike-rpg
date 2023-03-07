b3d9f8968f15609aad20e869736294fa019f0485
1amplant <icefly001@gmail.com>
2023-03-08 01:00:41 +0800

    import input handlers and action into the main file to allow avatar to move in the desired direction when any of the movement key is pressed

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 import tcod
 
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
+    event_handler = EventHandler()
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
-        player_x = int(screen_width / 2),
-        player_y = int(screen_height / 2),
         tileset=tileset,
         title="Roguelike project",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(player_x=1, player_y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="@")
 
             context.present(root_console)
 
+            root_console.clear()
+
             for event in tcod.event.wait():
-                if event.type == "QUIT":
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
                     raise SystemExit()
 
 
